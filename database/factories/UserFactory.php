958c6308c22fb20dcdd5cc30219246c467b2968e
Arondite <Aronditee@gmail.com>
2026-09-19 12:37:33 +0800

    style: apply Laravel Pint formatting

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/** @extends Factory<\App\Models\User> */
+/**
+ * @extends Factory<User>
+ */
 class UserFactory extends Factory
 {
     protected static ?string $password;
